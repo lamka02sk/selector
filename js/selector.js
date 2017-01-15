9fbca7541e48abab39ec438f34cc4bf5a110a07f
lamka02sk <lamka02sk@gmail.com>
2017-01-15 20:16:55 +0100

    Fixed some minor bugs and performance improvements.

--- js/selector.js
+++ js/selector.js
@@ -1,5 +1,3 @@
-var i;
-
 // Close selector options when click anywhere
 document.querySelector("html").addEventListener("click", turnOffSelector);
 
@@ -9,41 +7,45 @@ function turnOffSelector(event) {
         return false;
     }
 
-    var selects = document.querySelectorAll(".selector-options");
-    for(i = 0; i < selects.length; i++) {
+    let selects = document.querySelectorAll(".selector-options");
+    let selectsLen = selects.length;
+    for(i = 0; i < selectsLen; ++i) {
         selects[i].classList.remove("opened");
     }
 
-    event.preventDefault();
+    if(event.target.classList.contains("selector-part") || event.target.parentNode.classList.contains("selector-part")) {
+        event.preventDefault();
+    }
 
 }
 
 // Open and close selector
-var selectors = document.querySelectorAll(".selector-selected");
-
-for(i = 0; i < selectors.length; i++) {
+let selectors = document.querySelectorAll(".selector-selected");
+let selectorsLen = selectors.length;
+for(let i = 0; i < selectorsLen; ++i) {
     selectors[i].addEventListener("click", toggleSelect);
 }
 
 function toggleSelect(event) {
 
     // Get selector id
-    var parent = event.target.parentElement;
+    let parent = event.target.parentElement;
 
-    var selector = "#" + parent.id + " .selector-options";
+    let selector = "#" + parent.id + " .selector-options";
 
     // Close every selector options
-    var selectors = document.querySelectorAll(".selector-options");
-    for(i = 0; i < selectors.length; i++) {
+    let selectors = document.querySelectorAll(".selector-options");
+    let selectorsLen = selectors.length;
+    for(let i = 0; i < selectorsLen; ++i) {
 
         // Get selector id
-        var parentI = selectors[i].parentElement;
+        let parentI = selectors[i].parentElement;
 
         if(parent.id === parentI.id) {
             continue;
         }
 
-        var selectorI = "#" + parentI.id + " .selector-options";
+        let selectorI = "#" + parentI.id + " .selector-options";
 
         document.querySelector(selectorI).classList.remove("opened");
 
@@ -57,39 +59,41 @@ function toggleSelect(event) {
 }
 
 // Click option
-var options = document.querySelectorAll(".selector-option");
-
-for(i = 0; i < options.length; i++) {
+let options = document.querySelectorAll(".selector-option");
+let optionsLen = options.length;
+for(let i = 0; i < optionsLen; ++i) {
     options[i].addEventListener("click", chooseOption);
 }
 
 function chooseOption(event) {
 
     // Choose clicked option
-    var optionValue = this.getAttribute("data-value");
-    var optionContent = this.innerHTML;
-    var selectName = this.parentElement.parentElement.id;
-    var selectElement = document.querySelector("select[name=" + selectName + "]");
+    let optionValue = this.getAttribute("data-value");
+    let optionContent = this.innerHTML;
+    let selectName = this.parentElement.parentElement.id;
+    let selectElement = document.querySelector("select[name=" + selectName + "]");
 
     // Change selected option
-    var selectorSelectedWrapper = document.getElementById(selectName);
-    var selectorSelectedElement = selectorSelectedWrapper.childNodes[1];
+    let selectorSelectedWrapper = document.getElementById(selectName);
+    let selectorSelectedElement = selectorSelectedWrapper.childNodes[1];
 
     // Remove selected class from all options
-    var siblings = this.parentNode.childNodes;
+    let siblings = this.parentNode.childNodes;
 
     selectorSelectedElement.innerHTML = optionContent;
     selectorSelectedElement.setAttribute("data-value", optionValue);
 
-    var currentSelectOption = selectElement;
-    for(i = 0; i < selectElement.length; i++) {
+    let currentSelectOption = selectElement;
+    let selectElementLen = selectElement.length;
+    for(let i = 0; i < selectElementLen; ++i) {
         selectElement[i].removeAttribute("selected");
         if(selectElement[i].getAttribute("value") === optionValue) {
             currentSelectOption = selectElement[i];
         }
     }
 
-    for(i = 0; i < +siblings.length; i++) {
+    let siblingsLen = siblings.length;
+    for(let i = 0; i < siblingsLen; ++i) {
         if(i % 2 !== 0 && i !== 0) {
             siblings[i].removeAttribute("selected");
             if(siblings[i].getAttribute("data-value") === optionValue) {
