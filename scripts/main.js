84787f8375ecdf801ad8c979a5f102b416c9dd80
lamka02sk <lamka02sk@gmail.com>
2017-03-16 19:34:36 +0100

    Added CSS and rendering logic of simple dropdown is done.

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ function Selector(parameters) {
 
     // Variables
     this.elements = {};
+    this.currentElement = {};
     this.elementsStructure = {};
 
     // Selector Default Settings
@@ -53,14 +54,17 @@ Selector.prototype.getElements = function() {
  */
 Selector.prototype.createInstance = function(element) {
 
+    // Save current element
+    this.currentElement = element;
+
     // Save instance content
-    this.saveContent(element);
+    this.saveContent();
 
     // Render instance elements
     this.renderInstance();
 
     // Create events
-    this.createEvents();
+    //this.createEvents();
 
 };
 
@@ -68,9 +72,10 @@ Selector.prototype.createInstance = function(element) {
  * Selector saveContent Function
  * Stores user defined content and attributes for current element
  */
-Selector.prototype.saveContent = function(element) {
+Selector.prototype.saveContent = function() {
 
     // Save element name
+    let element = this.currentElement;
     let elementName = element.tagName.toLowerCase();
     this.elementsStructure[elementName] = {};
 
@@ -85,8 +90,122 @@ Selector.prototype.saveContent = function(element) {
         this.elementsStructure[elementName][i] = {};
         this.elementsStructure[elementName][i].tag = childElementName;
         let childAttributes = element[i].attributes;
+        this.elementsStructure[elementName][i].text = element[i].text;
         for(let j = 0; j < childAttributes.length; ++j)
             this.elementsStructure[elementName][i][childAttributes[j].name] = childAttributes[j].value;
     }
+
+};
+
+/**
+ * Selector renderInstance Function
+ * Render instance Selector element
+ */
+Selector.prototype.renderInstance = function() {
+
+    // Create parent instance element
+    let parent = this.renderParent();
+
+    // Create options instance element
+    let options = this.renderOptions();
+
+    // Create selected instance element
+    let selectedElement = options.querySelector('div.option-selected');
+    let selected = this.renderSelected(selectedElement);
+
+    // Connect parts together
+    parent.appendChild(selected);
+    parent.appendChild(options);
+
+    // Show element in browser and hide original user defined
+    this.currentElement.style.display = 'none';
+    this.currentElement.parentNode.insertBefore(parent, this.currentElement.nextSibling);
+
+};
+
+/**
+ * Selector renderParent Function
+ * Pre-renders parent instance element
+ * @returns {Element}
+ */
+Selector.prototype.renderParent = function() {
+
+    let parent = document.createElement('div');
+    parent.className = 'selector-element';
+    parent.dataset.reference = this.elementsStructure.select.name;
+    return parent;
+
+};
+
+/**
+ * Selector renderSelected Function
+ * Pre-renders selected instance element
+ * @returns {Element}
+ */
+Selector.prototype.renderSelected = function(selectedElement) {
+
+    let selected = document.createElement('div');
+    selected.className = 'selector-selected';
+    let selectedText = document.createElement('p');
+    selectedText.className = 'selected-text';
+
+    // Add attributes to selected element + content
+    selectedText.innerText = selectedElement.innerText;
+    for(let i in selectedElement.dataset)
+        selected.dataset[i] = selectedElement.dataset[i];
+
+    selected.appendChild(selectedText);
+    return selected;
+
+};
+
+/**
+ * Selector renderOptions Function
+ * Pre-renders options instance element
+ * @returns {Element}
+ */
+Selector.prototype.renderOptions = function() {
+
+    // Append each option to options
+    let options = document.createElement('div');
+    options.className = 'selector-options';
+    for(let i = 0; i < this.currentElement.childElementCount; ++i) {
+
+        let option = document.createElement('div');
+        option.className = 'selector-option';
+        let optionText;
+
+        for(let j in this.elementsStructure['select'][i]) {
+
+            let valueIndex = j;
+            if(j == 'tag') continue;
+
+            if(j == 'value')
+                j = 'data-value';
+
+            if(j == 'selected' || j == 'disabled') {
+                this.elementsStructure['select'][i][valueIndex] = 'true';
+                option.className += ' option-' + j;
+                continue;
+            }
+
+            if(j == 'text') {
+                optionText = document.createElement('span');
+                optionText.className = 'option-text';
+                optionText.innerText = this.elementsStructure['select'][i][valueIndex];
+                continue;
+            }
+
+            option.setAttribute(j, this.elementsStructure['select'][i][valueIndex]);
+
+        }
+
+        option.setAttribute('data-item', i);
+        option.appendChild(optionText);
+        options.appendChild(option);
+
+    }
+
+    return options;
 
 };
